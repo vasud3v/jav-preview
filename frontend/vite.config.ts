4daf8405ab1d13cc9f6c4d804e55ba3054b6c1a6
vasud3v <vasud3v@users.noreply.github.com>
2026-01-18 09:24:26 +0530

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import path from "path"
+import { fileURLToPath } from "url"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filepath);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
